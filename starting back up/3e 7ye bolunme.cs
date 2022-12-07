05a831ece8aa0193beff3069f3aee214a0c85b37
kazzhim <118839750+kazzhim@users.noreply.github.com>
2022-12-07 17:26:13 +0400

    Add files via upload

---
+++ starting back up/3e 7ye bolunme.cs
@@ -0,0 +1,15 @@
+﻿Console.Write("Reqem:");
+int n = Convert.ToInt32(Console.ReadLine());
+if(n<=0)
+{
+    Console.WriteLine("duzgunreqem daxil et");
+}
+else if(n%21!=0)
+{
+    Console.WriteLine("hem 3e hem 7ye bolunmur");
+
+}
+else
+{
+    Console.WriteLine("hem 3e hem 7ye bolunur");
+}
---
+++ starting back up/Program.cs
@@ -0,0 +1,35 @@
+﻿
+Console.Write("Reqem:");
+int point = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Neticeniz:");
+if (point < 0 && point > 100)
+{
+    Console.WriteLine("Duzgun reqem daxil et");
+
+}
+else if (point >= 91 && point <= 100)
+{
+    Console.WriteLine("A");
+}
+else if
+    (point >= 81 && point <= 90)
+{
+    Console.WriteLine("B");
+}
+else if (point >= 71 && point <= 80)
+{
+    Console.WriteLine("C");
+}
+else if (point >= 61 && point <= 70)
+{
+    Console.WriteLine("D");
+}
+else if (point >= 51 && point <= 60)
+{
+    Console.WriteLine("E");
+}
+else
+{
+    Console.WriteLine("Kesildiz");
+}
